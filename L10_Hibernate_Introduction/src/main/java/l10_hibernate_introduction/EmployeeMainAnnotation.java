eb63e24ff807f9f4397a006a5ae3732a66a13088
Surbhi <surbhikumaridav@gmail.com>
2023-12-11 18:28:14 +0530

    hibernate-annotation

---
+++ L10_Hibernate_Introduction/src/main/java/l10_hibernate_introduction/EmployeeMainAnnotation.java
@@ -0,0 +1,24 @@
+package l10_hibernate_introduction;
+
+import org.hibernate.Session;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import org.hibernate.Transaction;
+
+
+public class EmployeeMainAnnotation {
+	public static void main(String[] args) {
+		Configuration c=new Configuration();
+		SessionFactory sf=c.configure("hbm-annotation.cfg.xml").buildSessionFactory();
+		System.out.println(sf);
+		Session session=sf.openSession();
+		EmployeeAnnotation e=new EmployeeAnnotation(0,"Aditya",1000);
+		Transaction tx = session.beginTransaction();
+		session.save(e);
+		session.flush();
+		tx.commit();
+		session.close();
+	}
+}
