b2bb9853626807f0a1a4731f8b105845154ac86f
Surbhi <surbhikumaridav@gmail.com>
2023-12-28 01:45:40 +0530

    many to many xml

--- l11_hibernate_mapping/src/main/java/l11_hibernate_mapping/many2many/M2MMain.java
+++ l11_hibernate_mapping/src/main/java/l11_hibernate_mapping/many2many/M2MMain.java
@@ -16,21 +16,45 @@ public class M2MMain {
 		Session s = sf.openSession();
 		System.out.println("Session started:" + s);
 		Transaction tx = s.beginTransaction();
+		
 		Cart cart = new Cart();
 		cart.setName("Big Billion Day Cart");
+		
 		Items i1 = new Items("iphone", 20000);
-
 		Items i2 = new Items("macbook", 500000);
+		Items i3 = new Items("iphone pro", 200000);
+		Items i4 = new Items("macbook pro max", 5000000);
+		
 		Set<Items>itemSet=new HashSet<Items>();
 		itemSet.add(i1);
 		itemSet.add(i2);
 		
-		cart.setItems(itemSet);
-		cart.setTotal(100*2+50*5);
+		Set<Items>itemSet2=new HashSet<Items>();
+		itemSet2.add(i3);
+		itemSet2.add(i4);
 		
+		cart.setItems(itemSet);
+//		cart.setTotal(100*2+50*5);
+		
+		Cart cart1 = new Cart();
+		cart1.setName("Big Billion Day Cart-Amazon");
+		cart1.setItems(itemSet2);
+//		cart.setTotal(0);
+		
+		
+		Set<Cart>cartSet=new HashSet<Cart>();
+		cartSet.add(cart1);
+		cartSet.add(cart);
+		
+		
+		;
 		s.save(i1);
 		s.save(i2);
+		s.save(i3);
+		s.save(i4);
 		s.save(cart);
+		s.save(cart1);
+
 		
 		s.flush();
 		tx.commit();
