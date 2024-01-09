16e62c6cc0ecae2ebced5e055d586806ccc4c99c
Surbhi <surbhikumaridav@gmail.com>
2024-01-10 00:24:04 +0530

    aop springboot

---
+++ L14_SB_with_AOP/src/main/java/org/geeksforgeeks/jbdl60/L14SbWithAopApplication.java
@@ -0,0 +1,13 @@
+package org.geeksforgeeks.jbdl60;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L14SbWithAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L14SbWithAopApplication.class, args);
+	}
+
+}
