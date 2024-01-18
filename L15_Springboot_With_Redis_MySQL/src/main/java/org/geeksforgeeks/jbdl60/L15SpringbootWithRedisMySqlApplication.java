155e2059b55c3d4e12ffe0f9806d582b51084613
Surbhi <surbhikumaridav@gmail.com>
2024-01-19 00:54:37 +0530

    start springboot with redis and mysql using spring data jpa

---
+++ L15_Springboot_With_Redis_MySQL/src/main/java/org/geeksforgeeks/jbdl60/L15SpringbootWithRedisMySqlApplication.java
@@ -0,0 +1,13 @@
+package org.geeksforgeeks.jbdl60;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L15SpringbootWithRedisMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L15SpringbootWithRedisMySqlApplication.class, args);
+	}
+
+}
